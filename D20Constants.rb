0a6829aae288bba4db22a85b088968680c04cbed
Keiran Stockley <keirans@gmail.com>
2019-12-05 15:50:50 -0800

    moved D20 specific constants into their own file. Changed spacing and formatting of output for DnD-Dice

---
+++ D20Constants.rb
@@ -0,0 +1,26 @@
+require "./DieClass.rb"
+
+#generate the standard set of dice for the D20 roller
+D4 = Die.new 4
+D6 = Die.new 6
+D8 = Die.new 8
+D10 = Die.new 10
+D12 = Die.new 12
+D20 = Die.new 20
+D100 = Die.new 100
+
+#help documents for the D20 roller
+TUTORIAL = <<HELP
+\nUSAGE: Enter in the sequence of dice and/or modifiers you wish to calculate
+         DICE: [number of dice] followed by the die to roll (d4, d6, etc.)
+         MODIFIERS: [positive integer]
+         OPERATORS: [+, -, /, *]
+
+EXAMPLE: "3d6 + 2d8 - 6"\n
+
+SEE ALSO: dice\n
+HELP
+
+DICE_HELP = <<HELP
+\nThe D20 system uses the following dice: d4, d6, d8, d10, d12, d20, and d100\n
+HELP
