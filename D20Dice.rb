42f0821119f1eaba4ba3b88adb8a5729eab48639
Keiran Stockley <keirans@gmail.com>
2019-12-18 14:58:37 -0800

    reformatted output to display each die roll result and a final line tallying all results and constants

--- D20Dice.rb
+++ D20Dice.rb
@@ -8,6 +8,7 @@ def dnd_dice
     error = false
     sum = 0 #results sum starts at 0 for each roll
     operator = "+" #default to addition for the very first element
+    output = []
     
     print "Enter the dice to be rolled (3d6 + 2 for example), 'help' or 'exit': "
     input = gets.strip.split(' ') #separate each piece of the die roll
@@ -28,22 +29,27 @@ def dnd_dice
     input.each do |ele|
       if ele.match(/^\d+d(4||6||8||10||12||20||100)$/) #if the element is a die roll in <Integer>d<Integer> format
         result = roll_dice(ele.split('d')) #roll using number of dice and number of sides
+        output << result.sum
 
         case operator          #and apply the result(s) to <sum> via <operator>
         when "+"
-          sum += result
+          sum += result.sum
         when "-"
-          sum -= result
+          sum -= result.sum
+        when "/"
+          sum /= result.sum
+        when "*"
+          sum *= result.sum
         end
 
-        puts "\n\t#{ele}: #{result}"
+        puts "\n<\t#{ele}: #{result.join(" + ")} = #{result.sum}"
 
       elsif ele == "+" || ele == "-" #if the element is an arithmetic operator (+ or -)
         operator = ele         #store it in <operator>
-        puts "\n\t#{operator}"
+        output << operator
 
       elsif ele.match(/^\d+$/) #if the element is a constant
-        puts "\n\t#{ele}"
+        output << ele
 
         case operator          #apply the constant to <sum> via <operator>
         when "+"
@@ -60,8 +66,7 @@ def dnd_dice
       end
     end
     unless error
-      puts "\n\t="
-      puts "\nFinal sum: #{sum}\n\n"
+      puts "\n<\t#{output.join(' ')} = #{sum}"
     end
   end
 end
@@ -70,11 +75,11 @@ def roll_dice (dice)
   #convert the numbers of dice and sides to Integers
   number = dice[0].to_i
   sides = dice[1].to_i
-  result = 0
+  results = []
 
-  number.times { |i| result += rand(sides) + 1 }
+  number.times { |i| results << rand(sides) + 1 }
 
-  result
+  results
 end
 
 dnd_dice
